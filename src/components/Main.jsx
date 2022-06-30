8cbbae9028fb64cab06eceffdeaaafb2e5e29c82
özcan çetin <cetin445@gmail.com>
2022-06-30 16:04:26 +0300

    add handleSubmit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,45 @@
-import { useState } from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Main = () => {
-    const [searchText, setSearchText] = useState("")
-    const handleChange = (e) => {
-        setSearchText(e.target.value);
-        // console.log(searchText);
-    }
-    // console.log(searchText);
-    
-    return (
-      <section className="main">
-        <form>
-          <input onChange={handleChange} type="text" placeholder="Search for a city" autoFocus />
-          <button type="submit">SUBMIT</button>
-          <span className="msg"></span>
-        </form>
-        <div className="container">
-          <ul className="cities"></ul>
-        </div>
-      </section>
-    );
+  const [searchText, setSearchText] = useState("");
+  const handleChange = (e) => {
+    setSearchText(e.target.value);
   };
-  
-  export default Main;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getWeatherDataFromApi();
+  };
+  const getWeatherDataFromApi = async () => {
+    let apiKey = process.env.REACT_APP_API_KEY;
+    let units = "metric";
+    let lang = "tr";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${apiKey}&units=${units}&lang=${lang}`;
+    try {
+      const response = await axios.get(url);
+      console.log(response);
+      const { main, name, sys, weather} = response.data;
+      // const iconUrl = `https://openweathermap.org/img/wn/${
+      //         weather[0].icon}@2x.png`;
+    } catch {}
+  };
+  return (
+    <section className="main">
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="Search for a city"
+          autoFocus
+        />
+        <button type="submit">SUBMIT</button>
+        <span className="msg"></span>
+      </form>
+      <div className="container">
+        <ul className="cities">Main</ul>
+      </div>
+    </section>
+  );
+};
+
+export default Main;
